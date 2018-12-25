ca56437d60f954c3419b57a3c17706d4889a0cca
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-25 11:24:53 +0530

    Classified possible moves with their directions.

--- src/piece/queen.js
+++ src/piece/queen.js
@@ -1,37 +1,110 @@
+const { zip } = require("../util.js");
+
 class Queen {
-    constructor(position) {
-        this.position = position;
-        this.positiveIndexDiffs = [1, 2, 3, 4, 5, 6, 7];
-        this.negativeIndexDiffs = [-1, -2, -3, -4, -5, -6, -7];
-    }
+  constructor(position) {
+    this.position = position;
+    this.positiveIndexDiffs = [1, 2, 3, 4, 5, 6, 7];
+    this.negativeIndexDiffs = [-1, -2, -3, -4, -5, -6, -7];
+  }
 
-    zip(firstList, secondList) {
-        return firstList.map((element, index) =>
-            [element, secondList[index]]);
-    }
+  allPositionDiffs() {
+    const upRightDiffs = zip(this.positiveIndexDiffs, this.positiveIndexDiffs);
+    const upLeftDiffs = zip(this.negativeIndexDiffs, this.positiveIndexDiffs);
+    const bottomRightDiffs = zip(
+      this.positiveIndexDiffs,
+      this.negativeIndexDiffs
+    );
+    const bottomLeftDiffs = zip(
+      this.negativeIndexDiffs,
+      this.negativeIndexDiffs
+    );
+    const upSideDiffs = zip(new Array(8).fill(0), this.positiveIndexDiffs);
+    const downSideDiffs = zip(new Array(8).fill(0), this.negativeIndexDiffs);
+    const rightSideDiffs = zip(this.positiveIndexDiffs, new Array(8).fill(0));
+    const leftSideDiffs = zip(this.negativeIndexDiffs, new Array(8).fill(0));
+    return {
+      upRightDiffs,
+      upSideDiffs,
+      upLeftDiffs,
+      leftSideDiffs,
+      bottomLeftDiffs,
+      downSideDiffs,
+      bottomRightDiffs,
+      rightSideDiffs
+    };
+  }
 
-    allPositionDiffs() {
-        return this.zip(this.positiveIndexDiffs, this.positiveIndexDiffs).concat(
-            this.zip(this.negativeIndexDiffs, this.positiveIndexDiffs),
-            this.zip(this.positiveIndexDiffs, this.negativeIndexDiffs),
-            this.zip(this.negativeIndexDiffs, this.negativeIndexDiffs),
-            this.zip(new Array(8).fill(0), this.positiveIndexDiffs),
-            this.zip(new Array(8).fill(0), this.negativeIndexDiffs),
-            this.zip(this.positiveIndexDiffs, new Array(8).fill(0)),
-            this.zip(this.negativeIndexDiffs, new Array(8).fill(0))
-        ).sort();
-    }
+  placeMove(diff) {
+    return [this.position[0] + diff[0], this.position[1] + diff[1]];
+  }
 
-    allPossibleMoves() {
-        return this.allPositionDiffs().map(x =>
-            [this.position[0] + x[0], this.position[1] + x[1]]);
-    }
+  allPossibleMoves() {
+    const {
+      upRightDiffs,
+      upSideDiffs,
+      upLeftDiffs,
+      leftSideDiffs,
+      bottomLeftDiffs,
+      downSideDiffs,
+      bottomRightDiffs,
+      rightSideDiffs
+    } = this.allPositionDiffs();
+    const upRightMoves = upRightDiffs.map(this.placeMove, this);
+    const upSideMoves = upSideDiffs.map(this.placeMove, this);
+    const upLeftMoves = upLeftDiffs.map(this.placeMove, this);
+    const leftSideMoves = leftSideDiffs.map(this.placeMove, this);
+    const bottomLeftMoves = bottomLeftDiffs.map(this.placeMove, this);
+    const downSideMoves = downSideDiffs.map(this.placeMove, this);
+    const bottomRightMoves = bottomRightDiffs.map(this.placeMove, this);
+    const rightSideMoves = rightSideDiffs.map(this.placeMove, this);
+    return {
+      upRightMoves,
+      upSideMoves,
+      upLeftMoves,
+      leftSideMoves,
+      bottomLeftMoves,
+      downSideMoves,
+      bottomRightMoves,
+      rightSideMoves
+    };
+  }
 
-    validPossibleMoves() {
-        return this.allPossibleMoves().filter(cell =>
-            cell.every(index => index >= 0 && index < 8)
-        );
-    }
-};
+  filterMoves(move) {
+    return move.every(index => index >= 0 && index < 8);
+  }
 
+  validPossibleMoves() {
+    const {
+      upRightMoves,
+      upSideMoves,
+      upLeftMoves,
+      leftSideMoves,
+      bottomLeftMoves,
+      downSideMoves,
+      bottomRightMoves,
+      rightSideMoves
+    } = this.allPossibleMoves();
+    const validUpRightMoves = upRightMoves.filter(this.filterMoves);
+    const validUpSideMoves = upSideMoves.filter(this.filterMoves);
+    const validUpLeftMoves = upLeftMoves.filter(this.filterMoves);
+    const validLeftSideMoves = leftSideMoves.filter(this.filterMoves);
+    const validBottomLeftMoves = bottomLeftMoves.filter(this.filterMoves);
+    const validDownSideMoves = downSideMoves.filter(this.filterMoves);
+    const validBottomRightMoves = bottomRightMoves.filter(this.filterMoves);
+    const validRightSideMoves = rightSideMoves.filter(this.filterMoves);
+    return {
+      validUpRightMoves,
+      validUpSideMoves,
+      validUpLeftMoves,
+      validLeftSideMoves,
+      validBottomLeftMoves,
+      validDownSideMoves,
+      validBottomRightMoves,
+      validRightSideMoves
+    };
+  }
+}
+
+let queen = new Queen([0, 1]);
+console.log(queen.validPossibleMoves());
 exports.Queen = Queen;
--- test/pieceTest/queenTest.js
+++ test/pieceTest/queenTest.js
@@ -1,39 +1,46 @@
-const assert = require('assert');
-const { Queen } = require('../../src/piece/queen.js');
+const assert = require("assert");
+const { Queen } = require("../../src/piece/queen.js");
 
-describe('Queen', function () {
-    describe('validPossibleMoves', function () {
-        it('should return empty array of moves for postion is out of board', function () {
-            let queen = new Queen([-5, -11]);
-            let actualOutput = queen.validPossibleMoves();
-            assert.deepEqual(actualOutput, []);
-        })
-
-        it('should return all valid moves that are present for input [0,1]', function () {
-            let queen = new Queen([0, 1]);
-            let actualOutput = queen.validPossibleMoves();
-            let expectedOutput = [[0, 0],
-            [0, 2],
-            [0, 3],
-            [0, 4],
-            [0, 5],
-            [0, 6],
-            [0, 7],
-            [1, 0],
-            [1, 1],
-            [1, 2],
-            [2, 1],
-            [2, 3],
-            [3, 1],
-            [3, 4],
-            [4, 1],
-            [4, 5],
-            [5, 1],
-            [5, 6],
-            [6, 1],
-            [6, 7],
-            [7, 1]];
-            assert.deepEqual(actualOutput, expectedOutput);
-        })
+describe("Queen", function() {
+  describe("validPossibleMoves", function() {
+    it("should return empty array of moves for postion is out of board", function() {
+      let queen = new Queen([-5, -11]);
+      let actualOutput = queen.validPossibleMoves();
+      let expectedOutput = {
+        validUpRightMoves: [],
+        validUpSideMoves: [],
+        validUpLeftMoves: [],
+        validLeftSideMoves: [],
+        validBottomLeftMoves: [],
+        validDownSideMoves: [],
+        validBottomRightMoves: [],
+        validRightSideMoves: []
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
+
+    it("should return all valid moves that are present for input [0,1]", function() {
+      let queen = new Queen([0, 1]);
+      let actualOutput = queen.validPossibleMoves();
+      let expectedOutput = {
+        validUpRightMoves: [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]],
+        validUpSideMoves: [[0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]],
+        validUpLeftMoves: [],
+        validLeftSideMoves: [],
+        validBottomLeftMoves: [],
+        validDownSideMoves: [[0, 0]],
+        validBottomRightMoves: [[1, 0]],
+        validRightSideMoves: [
+          [1, 1],
+          [2, 1],
+          [3, 1],
+          [4, 1],
+          [5, 1],
+          [6, 1],
+          [7, 1]
+        ]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
+    });
+  });
 });
